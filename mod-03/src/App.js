fd5a649585ddbc36efe75f32158737542d8f970b
Maurice de Beijer <maurice.de.beijer@gmail.com>
2019-07-15 17:24:54 +0100

    React Router Dom

--- mod-03/src/App.js
+++ mod-03/src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "./App.css";
 
+import { Route, Switch, Link } from "react-router-dom";
+
 import Cat from "./components/Cat";
 import Dog from "./components/Dog";
 
@@ -8,13 +10,26 @@ function App() {
   return (
     <div>
       <header>
-        <a href="/cat">Cat</a>
+        <Link to="/cat">Cat</Link>
         <span> | </span>
-        <a href="/dog">Dog</a>
+        <Link to="/cat/Zorro">Cat</Link>
+        <span> | </span>
+        <Link to="/dog">Dog</Link>
       </header>
 
-      <Cat />
-      <Dog />
+      {/* <Cat />
+      <Dog /> */}
+      <Switch>
+        <Route path="/cat" exact component={Cat} />
+        <Route path="/cat/:name" exact component={Cat} />
+        <Route path="/dog" component={Dog} />
+        <Route
+          path="/"
+          exact
+          render={() => <div>The home page, make a choice</div>}
+        />
+        <Route render={() => <div>THis is not an animal</div>} />
+      </Switch>
     </div>
   );
 }
--- mod-03/src/components/Cat.js
+++ mod-03/src/components/Cat.js
@@ -1,9 +1,15 @@
 import React from "react";
 import imgSrc from "./cat.jpg";
 
-function Cat() {
+import { Prompt } from "react-router-dom";
+
+function Cat(props) {
+  console.log(props);
+
   return (
     <div>
+      <Prompt when={true} message="Are your sure?" />
+      <h1>{props.match.params.name}</h1>
       <img src={imgSrc} alt="A cat" height="200" />
     </div>
   );
